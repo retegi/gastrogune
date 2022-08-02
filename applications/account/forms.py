ac6f323e1d69b420198d52feb3d05f406b411acd
Iñaki Retegi <retegi84@gmail.com>
2022-08-03 00:14:07 +0200

    Mejoras en formularios

--- applications/account/forms.py
+++ applications/account/forms.py
@@ -5,6 +5,20 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self,*args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['username'].label = "Nombre de usuario / Erabiltzailea"
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].label = "Contraseña / Pasahitza"
+
+
+
+
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput)
@@ -14,6 +28,19 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+    
+    def __init__(self,*args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['username'].label = "Usuario / Erabiltzailea"
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].label = "Apellido / Abizena"
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].label = "Email / Emaila"
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].label = "Contraseña / Pasahitza"
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].label = "Repetir password / Errepikatu pasahitza"
 
     def clean_password2(self):
         cd = self.cleaned_data
